0fb13f70adc6f43c0dae9791b33d44fe417c6799
Bruno Coelho <eu.brunocoelho94@gmail.com>
2023-04-22 12:59:10 -0300

    finished

--- app/index.js
+++ app/index.js
@@ -26,13 +26,43 @@ const chapterReader = (URL) => {
         RenderView.openPage(elements, openPage);
         elements.optionsPage.innerHTML = optionsRender;
 
-        elements.optionsPage.addEventListener('change', (e) => { 
+        elements.body.addEventListener('keydown', (e) => {
+            let key = e.keyCode;
+
+            if (key == 37) {
+                e.preventDefault();
+
+                elements.chapterPage.scrollIntoView();
+
+                ReaderController.previousPage();
+                ReaderController.selectedPage(optionsPage, ReaderController.getPosition);
+
+                let openPage = ReaderController.openPage();
+
+                RenderView.openPage(elements, openPage);
+            }
+
+            if (key == 39) {
+                e.preventDefault();
+
+                elements.chapterPage.scrollIntoView();
+
+                ReaderController.nextPage();
+                ReaderController.selectedPage(optionsPage, ReaderController.getPosition);
+
+                let openPage = ReaderController.openPage();
+
+                RenderView.openPage(elements, openPage);
+            }
+        });
+
+        elements.optionsPage.addEventListener('change', (e) => {
             e.preventDefault();
 
             let selectedPage = elements.optionsPage.value;
 
             ReaderController.setPosition = selectedPage;
-            
+
             let openPage = ReaderController.openPage();
 
             RenderView.openPage(elements, openPage);
@@ -59,110 +89,20 @@ const chapterReader = (URL) => {
 
             RenderView.openPage(elements, openPage);
         });
+
+        elements.chapterPage.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            elements.chapterPage.scrollIntoView();
+
+            ReaderController.nextPage();
+            ReaderController.selectedPage(elements.optionsPage, ReaderController.getPosition);
+
+            let openPage = ReaderController.openPage();
+
+            RenderView.openPage(elements, openPage);
+        })
     }
 }
 
 chapterReader(chapterURL);
-
-// let init = (chapter) => {
-//     const
-//         ReaderController = new Reader(chapter),
-//         RenderView = new Render();
-
-//     let
-//         chapterPages = ReaderController.getChapterPages,
-//         numberPages = ReaderController.getNumberPages,
-//         optionsRender = RenderView.optionsPage(chapterPages, numberPages);
-
-//     const
-//         body = document.querySelector('body'),
-//         previousButton = document.querySelector('#previous'),
-//         nextButton = document.querySelector('#next'),
-//         optionsPage = document.querySelector('#optionsPage');
-
-//     let
-//         expandPage = document.querySelector('#expand'),
-//         chapterPage = document.querySelector('#chapterPage');
-
-//     let openPage = ReaderController.openPage();
-
-//     const generateElements = {
-//         expandPage: expandPage,
-//         chapterPage: chapterPage
-//     };
-
-//     RenderView.openPage(generateElements, openPage);
-
-//     optionsPage.innerHTML = optionsRender;
-
-//     body.addEventListener('keydown', (e) => {
-//         let key = e.keyCode;
-
-//         if (key == 37) {
-//             e.preventDefault();
-
-//             ReaderController.previousPage();
-//             ReaderController.selectedPage(optionsPage, ReaderController.position);
-
-//             let openPage = ReaderController.openPage();
-
-//             RenderView.openPage(generateElements, openPage);
-//         }
-
-//         if (key == 39) {
-//             e.preventDefault();
-
-//             ReaderController.nextPage();
-//             ReaderController.selectedPage(optionsPage, ReaderController.position);
-
-//             let openPage = ReaderController.openPage();
-
-//             RenderView.openPage(generateElements, openPage);
-//         }
-//     });
-
-//     chapterPage.addEventListener('click', (e) => {
-//         e.preventDefault();
-
-//         ReaderController.nextPage();
-//         ReaderController.selectedPage(optionsPage, ReaderController.position);
-
-//         let openPage = ReaderController.openPage();
-
-//         RenderView.openPage(generateElements, openPage);
-//     });
-
-//     optionsPage.addEventListener('change', (e) => {
-//         e.preventDefault();
-
-//         let selectedPage = optionsPage.value;
-
-//         ReaderController.position = selectedPage;
-
-//         let openPage = ReaderController.openPage();
-
-//         RenderView.openPage(generateElements, openPage);
-//     });
-
-//     previousButton.addEventListener('click', (e) => {
-//         e.preventDefault();
-
-//         ReaderController.previousPage();
-//         ReaderController.selectedPage(optionsPage, ReaderController.position);
-
-//         let openPage = ReaderController.openPage();
-
-//         RenderView.openPage(generateElements, openPage);
-//     });
-
-//     nextButton.addEventListener('click', (e) => {
-//         e.preventDefault();
-
-//         ReaderController.nextPage();
-//         ReaderController.selectedPage(optionsPage, ReaderController.position);
-
-//         let openPage = ReaderController.openPage();
-
-//         RenderView.openPage(generateElements, openPage);
-//     });
-// }
